4450d19a00702fdee6cc758779c076136e9a5ad1
Max Penet <m@qbits.cc>
2016-07-27 12:59:39 +0200

    bump v

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.qbits/spex "0.1.0-SNAPSHOT"
+(defproject cc.qbits/spex "0.1.0"
   :description "Simple spex extensions, utils"
   :url "https://github.com/mpenet/spex"
   :license {:name "Eclipse Public License"
