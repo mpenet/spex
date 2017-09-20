4a680c071620e29970ecdb3fb33254039fd50b14
Max Penet <m@qbits.cc>
2017-09-20 13:12:30 +0200

    bump clj

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/mpenet/spex"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta1"]
                  [org.clojure/test.check "0.9.0"]
                  [com.gfredericks/test.chuck "0.2.6"]]
   :source-paths ["src/clj"]
