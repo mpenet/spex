5148c14f535cdbabf71c1d5b577504d9f5d68640
Max Penet <max.penet@exoscale.ch>
2019-11-21 16:02:16 +0100

    Version 0.7.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.qbits/spex "0.7.1-SNAPSHOT"
+(defproject cc.qbits/spex "0.7.1"
   :description "Simple spex extensions, utils"
   :url "https://github.com/mpenet/spex"
   :license {:name "Eclipse Public License"
